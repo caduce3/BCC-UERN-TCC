fd9d8afd9576c9296671a259a211d5660f694a5f
Carlos Eduardo <cadu.ce3@gmail.com>
2023-10-10 22:39:46 -0300

    fix: ajustando forma de leitura para os áudios

--- python-speaks/main3.py
+++ python-speaks/main3.py
@@ -7,7 +7,7 @@ import os
 pygame.init()
 
 # Configurando a porta serial
-ser = serial.Serial('COM6', 19200)
+ser = serial.Serial('COM5', 19200)
 
 # Opções de áudio
 audio_options = [
@@ -17,42 +17,45 @@ audio_options = [
     'roboldo04.mp3'
 ]
 
+# Caminho absoluto para o diretório dos arquivos de áudio
+audio_directory = r'C:\Users\caduc\Documents\BCC-UERN-TCC\python-speaks'
+
 # Loop principal
 while True:
     # Lendo a porta serial
     if ser.in_waiting > 0:
         serial_input = ser.readline().decode('utf-8').rstrip()
         if serial_input == "play_audio":
-            # Carregando o arquivo de áudio
+            # Carregando o arquivo de áudio de entrada
             audio_file = "roboldoEntradaW.mp3"
-            audio_path = os.path.join(os.getcwd(), audio_file)
-            print("MEU ARQUIVO >>>>> "+audio_path)
+            audio_path = os.path.join(audio_directory, audio_file)
+            print("MEU ARQUIVO >>>>> " + audio_path)
             if os.path.exists(audio_path):
                 pygame.mixer.music.load(audio_path)
                 pygame.mixer.music.set_volume(1)
             else:
-                print('O arquivo ' + audio_file + ' não está no diretório do script Python')
+                print('O arquivo ' + audio_file + ' não está no diretório de áudio')
 
             pygame.mixer.music.play()
             # time.sleep(30)  # espera 30 segundos antes de continuar com a execução do programa
         elif serial_input == "play_audio_fim":
-            # Carregando o arquivo de áudio
+            # Carregando o arquivo de áudio de saída
             audio_file = "roboldoSaidaW.mp3"
-            audio_path = os.path.join(os.getcwd(), audio_file)
-            print("MEU ARQUIVO >>>>> "+audio_path)
+            audio_path = os.path.join(audio_directory, audio_file)
+            print("MEU ARQUIVO >>>>> " + audio_path)
             if os.path.exists(audio_path):
                 pygame.mixer.music.load(audio_path)
                 pygame.mixer.music.set_volume(1)
             else:
-                print('O arquivo ' + audio_file + ' não está no diretório do script Python')
+                print('O arquivo ' + audio_file + ' não está no diretório de áudio')
 
             pygame.mixer.music.play()
             # time.sleep(30)  # espera 30 segundos antes de continuar com a execução do programa
         elif serial_input == "aprsEmocao":
             # Selecionando o arquivo de áudio a ser tocado
             audio_file = random.choice(audio_options)
-            audio_path = os.path.join('C:', os.sep, 'Users', 'danie', 'Downloads', 'BCC-UERN-TCC-master', 'speaks', audio_file)
-            print("MEU ARQUIVO >>>>> "+audio_path)
+            audio_path = os.path.join(audio_directory, audio_file)
+            print("MEU ARQUIVO >>>>> " + audio_path)
             if os.path.exists(audio_path):
                 pygame.mixer.music.load(audio_path)
                 volume_atual = pygame.mixer.music.get_volume()
@@ -61,4 +64,4 @@ while True:
                 while pygame.mixer.music.get_busy():
                     pass
             else:
-                print('O arquivo ' + audio_file + ' não está no diretório do script Python')
+                print('O arquivo ' + audio_file + ' não está no diretório de áudio')
